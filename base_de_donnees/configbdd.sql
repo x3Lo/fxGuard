95b8ad32ac1e04a03caefefbf300cbd0703a2412
Edern <edern.ferlicot@gmail.com>
2025-04-23 16:17:12 +0200

    ajout du dossier de création

---
+++ base_de_donnees/configbdd.sql
@@ -0,0 +1,11 @@
+INSERT INTO user_ (userName, email, password, role)
+VALUES ('admin', 'admin@example.com', '$2y$10$cypJjXGzKCgGW5VmmdySuuHM0MQ5ezcGPIXJWgHwGq8XIk81zjItS', 'admin');
+
+INSERT INTO theme (themeName) 
+VALUE 
+    ('sport'),
+    ('off road'),
+    ('retro'),
+    ('cabriolet'),
+    ('Coupé'),
+    ('berline');
